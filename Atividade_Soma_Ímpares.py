a2557d67aa6a531d315b319fa6593994eccfe4d5
Janaina Rocha <113792506+DevJanainaRocha@users.noreply.github.com>
2024-09-29 09:47:00 -0300

    Update Atividade_Soma_Ímpares.py

--- Atividade_Soma_Ímpares.py
+++ Atividade_Soma_Ímpares.py
@@ -1,5 +1,4 @@
 print('Soma dos ímpares de 1 até 10:\n')
-#Estudar esse comando
 #O segundo argumento de da função range é exclusivo, então range(1, 11) inclui números de 1 a 10) 
 #com um passo de 2 (o terceiro argumento de range). 
 #Isso garante que apenas números ímpares sejam considerados.
